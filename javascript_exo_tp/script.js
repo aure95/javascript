3546f4565877dd3ff737f12f0961234ec10cd62a
aure <mouretaurelien95git@gmail.com>
2018-06-27 10:40:27 +0200

    ihuiehti

--- javascript_exo_tp/script.js
+++ javascript_exo_tp/script.js
@@ -1,33 +1,96 @@
 
+//rafarichit la page
+var refresh=false;
+var etat="";
+
+function reload()
+{
+	location.reload();
+}
 
 function supprimer()
 {
-   
+   modifierEtat("SUPPRIMER");
+   var button_supprimer=document.getElementById("button_supprimer").style.color="green";
+   document.getElementById("button_modifier").style.color="red";
+}
+
+function modifierEtat(message)
+{
+	if(message=="ETAT")
+	var affichage_etat=document.getElementById("etat").innerHTML=message+" UNDEFINED";
+	if(message=="SUPPRIMER" || message=="MODIFIER")
+	var affichage_etat=document.getElementById("etat").innerHTML="ETAT "+message;
+
 }
 
 function ajouter()
 {
-  console.log("clicked");
+  
+  console.log("ajouter clicked");
+
+  modifierEtat("AJOUTER");
+ var data= document.getElementById("textAjout").value;
+  localStorage.setItem(getCle().toString(),data);
+  data.value="";
+  refresh=true;
+  reload();
 
   
+
 
 
 }
 
 function modifier()
 {
-
+ 	modifierEtat("MODIFIER");
+ 	var button_supprimer=document.getElementById("button_modifier").style.color="green";
+ 	document.getElementById("button_supprimer").style.color="red";
 }
 
 function affichage()
 {
-	var res="<br>totototo</br>";
-	for(var i=0;i>getCle();i++)
-		document.getElementById('affichage').innerHTML+=res;
-		
+    
+    
+	var max=localStorage.getItem("cle");
+	var max_int=parseInt(max);
+	for(var i=1;i<max_int+1;i++)
+	{
+
+	var button=document.createElement("input");
+	button.type="button";
+	button.value=localStorage.getItem(i.toString());
+	button.id=localStorage.getItem(i.toString());
+	var affichage=document.getElementById("affichage");
+
+/*
+	var button_modifier=document.createElement("input");
+	button_modifier.type="button";
+	button_modifier.value="modifier";
+
+	*/
+
+	affichage.innerHTML+="<br>";
+	affichage.appendChild(button);
+	//affichage.appendChild(button_modifier);
+
 
 	
-	console.log(res);
+
+	
+	}
+
+
+
+
+
+	/*for(var i=0;i>getCle();i++)
+		document.getElementById('affichage').innerHTML+=res;
+		
+*/
+	
+	console.log("gjithju");
 }
 
 function initGestionCle()
@@ -61,18 +124,26 @@ function getCle()
 
 function resetCle()
 {
+	/*
 	localStorage.removeItem("init");
 	localStorage.removeItem("cle");
+	//var nb_cle=getCle();
+	//for(var i=0;i<nb_cle;i++)
+		removeItem()
+	*/
+	localStorage.clear();
 }
 
 
 
-initGestionCle()
-getCle()
-getCle()
-getCle()
-getCle()
-affichage()
+
+initGestionCle();
+modifierEtat("ETAT");
+affichage();
+
+//
+
+//affichage()
 //resetCle()
 
 
